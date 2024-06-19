327348d2906a3e45e4d4f947131598956e1120ee
cain9209 <zacharylcain@gmail.com>
2024-06-19 17:28:42 -0500

    @RequestMapping @PostMapping @GetMapping Handler methods forms/ not templates yet

--- src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -13,34 +13,35 @@ public class HelloController {
     public String welcomePage() {
         return "Welcome to LaunchCode!";
     }
-// handles request that are not "Zack" //
+
+    // handles request that are not "Zack" //
     @GetMapping("goodbye")
     public String returnGoodbye() {
         return "Thank you for visiting LaunchCode please leave!";
     }
 
-// We have to create a form this way until templates are introduced later on//
-@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-public String helloWithQueryParam(@RequestParam String name) {
-    if (name.equals("Zack")) {
-        return "Hello " + name + " Welcome to learning Get and Post request!";
-    } else {
-        return returnGoodbye();
+    // We have to create a form this way until templates are introduced later on//
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name) {
+        if (name.equals("Zack")) {
+            return "Hello " + name + " Welcome to learning Get and Post request!";
+        } else {
+            return returnGoodbye();
+        }
+
     }
 
-}
-
-// Form get info from /hello and then post the info back to /hello hence the @RequestMapping//
-@GetMapping("form")
-@ResponseBody
-public String helloForm() {
-    return "<html>" +
-            "<body>" +
-            "<form action='hello' method='Post'>" +
-            "<input type='text' name='name'>" +
-            "<input type='submit' value='Greet Me'>" +
-            "</form>" +
-            "</body>" +
-            "</html>";
-}
+    // Form get info from /hello and then post the info back to /hello hence the @RequestMapping//
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='Post'>" +
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet Me'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
