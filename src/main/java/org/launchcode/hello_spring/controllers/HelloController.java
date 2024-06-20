f940e708bbe6a6046a78551a9c4b214f2f5f58c9
cain9209 <zacharylcain@gmail.com>
2024-06-20 18:51:56 -0500

    Learned how to add static template and return to the view hooray

--- src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -6,42 +6,34 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
-    @GetMapping //Get request calls method welcomePage then returns with @ResponceBody//
+    @GetMapping
+    @ResponseBody//Get request calls method welcomePage then returns with @ResponceBody//
     public String welcomePage() {
         return "Welcome to LaunchCode!";
     }
 
     // handles request that are not "Zack" //
     @GetMapping("goodbye")
+    @ResponseBody
     public String returnGoodbye() {
         return "Thank you for visiting LaunchCode please leave!";
     }
 
     // We have to create a form this way until templates are introduced later on//
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST},value="hello")
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
-        if (name.equals("Zack")) {
-            return "Hello " + name + " Welcome to learning Get and Post request!";
-        } else {
-            return returnGoodbye();
-        }
+        return "Hello " + name + " Welcome to learning Get and Post request!";
+
 
     }
 
     // Form get info from /hello and then post the info back to /hello hence the @RequestMapping//
     @GetMapping("form")
-    @ResponseBody
-    public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='Post'>" +
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet Me'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    public String getForm() {
+        return "form"; // return name of the static template //
     }
 }
